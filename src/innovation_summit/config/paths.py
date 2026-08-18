9e7688145587711e57e627fa7045405c6897d512
Corey Michaud <cor3y.michaud@gmail.com>
2026-08-18 14:48:33 -0400

    created config/ folder for better structure

--- src/innovation_summit/config/paths.py
+++ src/innovation_summit/config/paths.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parents[2]
+PROJECT_ROOT = Path(__file__).resolve().parents[3]
 
 RAW_DATA_PATH = PROJECT_ROOT / "data" / "raw"
 WAREHOUSE_PATH = PROJECT_ROOT / "data" / "warehouse"
