7f0b3e6b1ef3d8ef111679b1fd5b2980e2d84d4f
Corey Michaud <cor3y.michaud@gmail.com>
2026-08-18 10:42:49 -0400

    Added data warehouse

--- src/innovation_summit/config.py
+++ src/innovation_summit/config.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
-DATA_PATH = PROJECT_ROOT / "data"
-RAW_DATA_PATH = DATA_PATH / "raw"
-COMBINED_DATA_PATH = DATA_PATH / "combined"
+# DATA_PATH = PROJECT_ROOT / "data"
+# RAW_DATA_PATH = DATA_PATH / "raw"
+# COMBINED_DATA_PATH = DATA_PATH / "combined"
+WAREHOUSE_PATH = PROJECT_ROOT / "data" / "warehouse"
+DB_PATH = WAREHOUSE_PATH / "form_5500.db"
