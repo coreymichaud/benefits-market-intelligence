0d048fa36ec156a8991859a6b5ec0a85466bfcfa
Corey Michaud <cor3y.michaud@gmail.com>
2026-08-27 10:29:11 -0400

    feat: added  path

--- src/innovation_summit/config/paths.py
+++ src/innovation_summit/config/paths.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).resolve().parents[3]
 
-RAW_DATA_PATH = PROJECT_ROOT / "data" / "raw"
-PROCESSED_PATH = PROJECT_ROOT / "data" / "processed"
+DATA_PATH = PROJECT_ROOT / "data"
+
+RAW_DATA_PATH = DATA_PATH / "raw"
+PROCESSED_PATH = DATA_PATH / "processed"
 DB_PATH = PROCESSED_PATH / "form_5500.duckdb"
+EXPORTS_PATH = DATA_PATH / "exports"
